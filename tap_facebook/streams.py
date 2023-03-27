ea098b923af8225f5e50cad6e241a768ea1e4c32
edward <edward@ryan-miranda.com>
2023-03-27 15:57:18 -0700

    beginning ads insights stream, removing unused dbt formatted child streams from streams.py

--- tap_facebook/streams.py
+++ tap_facebook/streams.py
@@ -80,7 +80,24 @@ class adsinsightStream(facebookStream):
 
     name = "adsinsights"
     path = "/insights?level=ad&fields={}".format(columns)
-    schema_filepath = SCHEMAS_DIR / "ads_insights.json"
+    #schema_filepath = SCHEMAS_DIR / "ads_insights.json"
+    schema = th.PropertiesList(
+        Property("clicks", StringType),
+        Property("date_stop", StringType),
+        Property("ad_id", StringType),
+        Property("website_ctr",
+
+                 ArrayType(
+                     ObjectType(
+
+
+
+                     )
+                 )),
+
+
+    )
+
     tap_stream_id = "adsinsights"
     #replication_key = "created_time"
 
@@ -254,10 +271,12 @@ class campaignStream(facebookStream):
                      Property(
                          "data",
                          ArrayType(
-                         ObjectType(
-                             Property("id",StringType),
+                             ObjectType(
+                                 Property("id",
+                                          StringType
+                                          ),
+                             )
                          )
-                     )
                      )
                  )
                  ),
@@ -272,333 +291,4 @@ class campaignStream(facebookStream):
                  )
                  )
     ).to_dict()
-
-
-class adhistoryStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "adset_id",
-               "campaign_id",
-               "creative",
-               "bid_amount",
-               "bid_type",
-               "bid_info",
-               "status",
-               "conversion_domain",
-               "configured_status",
-               "effective_status",
-               "updated_time",
-               "created_time",
-               "name",
-               "last_updated_by_app_id",
-               "source_ad_id"]
-
-    name = "ad_history"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adhistory.json"
-    tap_stream_id = "ad_history"
-
-class campaignhistoryStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "updated_time",
-               "created_time",
-               "start_time",
-               "stop_time",
-               "name",
-               "bid_strategy",
-               "boosted_object_id",
-               "ad_strategy_group_id",
-               "ad_strategy_id",
-               "budget_rebalance_flag",
-               "buying_type",
-               "daily_budget",
-               "lifetime_budget",
-               "budget_remaining",
-               "can_create_brand_lift_study",
-               "can_use_spend_cap",
-               "configured_status",
-               "effective_status",
-               "has_secondary_skadnetwork_reporting",
-               "is_skadnetwork_attribution",
-               "last_budget_toggling_time",
-               "objective",
-               "pacing_type",
-               "promoted_object_application_type",
-               "promoted_object_custom_conversion_id",
-               "promoted_object_custom_event_str",
-               "promoted_object_custom_event_type",
-               "promoted_object_event_id",
-               "promoted_object_object_store_url",
-               "promoted_object_offer_id",
-               "promoted_object_offline_conversion_data_set_id",
-               "promoted_object_page_id",
-               "promoted_object_pixel_aggregation_rule",
-               "promoted_object_pixel_id",
-               "promoted_object_pixel_rule",
-               "promoted_object_place_page_set_id",
-               "promoted_object_product_catalog_id",
-               "promoted_object_product_set_id",
-               "promoted_object_retention_days",
-               "primary_attribution",
-               "smart_promotion_type",
-               "source_campaign_id",
-               "special_ad_categories",
-               "special_ad_category",
-               "special_ad_category_country",
-               "spend_cap",
-               "promoted_object",
-               "status",
-               "topline_id"]
-
-    name = "campaign_history"
-    path = "/campaigns?fields={}".format(columns)
-    schema_filepath = SCHEMAS_DIR / "campaignhistory.json"
-    tap_stream_id = "campaign_history"
-
-class adsethistoryStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "campaign_id",
-               "updated_time",
-               "created_time",
-               "start_time",
-               "end_time",
-               "name",
-               "asset_feed_id",
-               "bid_amount",
-               "bid_info_actions",
-               "bid_info_impressions",
-               "bid_strategy",
-               "billing_event",
-               "budget_remaining",
-               "configured_status",
-               "effective_status",
-               "daily_budget",
-               "lifetime_budget",
-               "daily_min_spend_target",
-               "daily_spend_cap",
-               "destination_type",
-               "instagram_actor_id",
-               "is_dynamic_creative",
-               "learning_stage_info",
-               "lifetime_imps",
-               "lifetime_min_spend_target",
-               "lifetime_spend_cap",
-               "multi_optimization_goal_weight",
-               "optimization_goal",
-               "optimization_sub_event",
-               "promoted_object_application_type",
-               "promoted_object_custom_conversion_id",
-               "promoted_object_custom_event_str",
-               "promoted_object_custom_event_type",
-               "promoted_object_event_id",
-               "promoted_object_object_store_url",
-               "promoted_object_offer_id",
-               "promoted_object_offline_conversion_data_set_id",
-               "promoted_object_page_id",
-               "promoted_object_pixel_aggregation_rule",
-               "promoted_object_pixel_id",
-               "promoted_object_pixel_rule",
-               "promoted_object_place_page_set_id",
-               "promoted_object_product_catalog_id",
-               "promoted_object_product_set_id",
-               "promoted_object_retention_days",
-               "recurring_budget_semantics",
-               "review_feedback",
-               "rf_prediction_id",
-               "promoted_object",
-               "attribution_spec",
-               "campaign_attribution",
-               "pacing_type",
-               "source_adset_id",
-               "status"]
-
-    name = "ad_set_history"
-    path = "/adsets?fields={}".format(columns)
-    schema_filepath = SCHEMAS_DIR / "adsethistory.json"
-    tap_stream_id = "ad_set_history"
-
-class adconversionStream(facebookStream):
-    columns = ["id",
-               "adset_id",
-               "campaign_id",
-               "conversion_id"
-               "application",
-               "creative",
-               "dataset",
-               "event",
-               "event_creator",
-               "event_type",
-               "fb_pixel",
-               "fb_pixel_event",
-               "index",
-               "leadgen",
-               "object",
-               "object_domain",
-               "offer",
-               "offer_creator",
-               "offsite_pixel",
-               "page",
-               "page_parent",
-               "post",
-               "post_object",
-               "post_object_wall",
-               "post_wall",
-               "question",
-               "question_creator",
-               "response",
-               "subtype",
-               "updated_time",
-               "created_time"]
-
-    name = "ad_conversion"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adconversion.json"
-    tap_stream_id = "ad_conversion"
-
-class campaignlabelStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "ad_label_id"
-               "source_campaign_id",
-               "adlabels"
-               "updated_time",
-               "created_time"]
-
-    name = "campaign_label"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "campaignlabel.json"
-    tap_stream_id = "campaign_label"
-
-class adgroupissuesinfoStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "error_code"
-               "error_message",
-               "error_summary",
-               "error_type",
-               "index",
-               "level",
-               "updated_time",
-               "created_time"]
-
-    name = "ad_group_issues_info"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adgroupissuesinfo.json"
-    tap_stream_id = "ad_group_issues_info"
-
-class adrecommendationStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "blame_field"
-               "code",
-               "confidence",
-               "importance",
-               "index",
-               "message",
-               "recommendation_data",
-               "title",
-               "updated_time",
-               "created_time"]
-
-    name = "ad_recommendation"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adrecommendation.json"
-    tap_stream_id = "ad_recommendation"
-
-class adsetscheduleStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "days",
-               "start_minute",
-               "end_minute",
-               "index",
-               "timezone_type",
-               "updated_time",
-               "created_time"]
-
-    name = "adset_schedule"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adsetschedule.json"
-    tap_stream_id = "adset_schedule"
-
-class adcampaignissuesinfoStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "error_code"
-               "error_message",
-               "error_summary",
-               "error_type",
-               "index",
-               "level",
-               "updated_time",
-               "created_time"]
-
-    name = "ad_campaign_issues_info"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adcampaignissuesinfo.json"
-    tap_stream_id = "ad_campaign_issues_info"
-
-class adsetattributionStream(facebookStream):
-    columns = ["id",
-               "account_id",
-               "event_type",
-               "index",
-               "window_days",
-               "updated_time",
-               "created_time"]
-
-    name = "ad_set_attribution"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adsetattribution.json"
-    tap_stream_id = "ad_set_attribution"
-
-class adtrackingStream(facebookStream):
-    columns = ["id",
-               "adset_id",
-               "campaign_id",
-               "conversion_id"
-               "application",
-               "creative",
-               "dataset",
-               "event",
-               "event_creator",
-               "event_type",
-               "fb_pixel",
-               "fb_pixel_event",
-               "index",
-               "leadgen",
-               "object",
-               "object_domain",
-               "offer",
-               "offer_creator",
-               "offsite_pixel",
-               "page",
-               "page_parent",
-               "post",
-               "post_object",
-               "post_object_wall",
-               "post_wall",
-               "question",
-               "question_creator",
-               "response",
-               "subtype",
-               "updated_time",
-               "created_time"]
-
-    name = "ad_tracking"
-    path = "/ads?fields={}".format(columns)
-    primary_keys = ["id"]
-    schema_filepath = SCHEMAS_DIR / "adtracking.json"
-    tap_stream_id = "ad_tracking"
-
 
