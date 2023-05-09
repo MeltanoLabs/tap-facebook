2e90b5615ede6c55d4f299d0ef41e389d93f01db
Dhrruv <dhrruv@Dhrruvs-MBP.bbrouter>
2023-05-09 19:37:52 +0530

    updated facebook pytest

--- tap_facebook/tests/test_core.py
+++ tap_facebook/tests/test_core.py
@@ -6,16 +6,23 @@ from singer_sdk.testing import get_standard_tap_tests
 
 from tap_facebook.tap import Tapfacebook
 
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+facebook_config = {
+    "start_date": os.getenv("TAP_FACEBOOK_START_DATE"),
+    "access_token":os.getenv("TAP_FACEBOOK_ACCESS_TOKEN"),
+    "account_id":os.getenv("TAP_FACEBOOK_ACCOUNT_ID"),
+    "api_version":"v16.0"
 }
 
 
 # Run standard built-in tap tests from the SDK:
 def test_standard_tap_tests():
     """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(Tapfacebook, config=SAMPLE_CONFIG)
+    tests = get_standard_tap_tests(Tapfacebook, config=facebook_config)
     for test in tests:
         test()
 
