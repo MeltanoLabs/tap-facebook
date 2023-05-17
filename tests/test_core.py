e28c19bb459c55db1a90912133aae26236ff3892
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-05-16 18:01:14 -0600

    test: Use new Singer SDK testing framework

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,24 +1,13 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
 
-from singer_sdk.testing import get_standard_tap_tests
+from singer_sdk.testing import get_tap_test_class
 
 from tap_facebook.tap import Tapfacebook
 
 SAMPLE_CONFIG = {
     "start_date": "2023-03-01T00:00:00Z",
-    "access_token": "abc123",
-    "account_id": "1000",
     "api_version": "v16.0",
 }
 
-
-# Run standard built-in tap tests from the SDK:
-def test_standard_tap_tests():
-    """Run standard tap tests from the SDK."""
-    tests = get_standard_tap_tests(Tapfacebook, config=SAMPLE_CONFIG)
-    for test in tests:
-        test()
-
-
-# TODO: Create additional tests as appropriate for your tap.
+TestTapFacebook = get_tap_test_class(Tapfacebook, config=SAMPLE_CONFIG)
