efd15d7510fedac60b4c16024ce2d9fe9e88543e
pnadolny13 <patnadolny@gmail.com>
2024-01-29 17:30:33 -0500

    exclude fields to make requests pass

--- tap_facebook/streams/ad_insights.py
+++ tap_facebook/streams/ad_insights.py
@@ -2,12 +2,14 @@
 
 from __future__ import annotations
 
+import time
 import typing as t
 from functools import lru_cache
 
 import facebook_business.adobjects.user as fb_user
 import pendulum
 from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.adobjects.adreportrun import AdReportRun
 from facebook_business.adobjects.adsactionstats import AdsActionStats
 from facebook_business.adobjects.adshistogramstats import AdsHistogramStats
 from facebook_business.adobjects.adsinsights import AdsInsights
@@ -24,6 +26,35 @@ from singer_sdk.typing import (
     StringType,
 )
 
+EXCLUDED_FIELDS = [
+    "total_postbacks",
+    "adset_end",
+    "adset_start",
+    "conversion_lead_rate",
+    "cost_per_conversion_lead",
+    "cost_per_dda_countby_convs",
+    "cost_per_one_thousand_ad_impression",
+    "cost_per_unique_conversion",
+    "creative_media_type",
+    "dda_countby_convs",
+    "dda_results",
+    "instagram_upcoming_event_reminders_set",
+    "interactive_component_tap",
+    "marketing_messages_cost_per_delivered",
+    "marketing_messages_cost_per_link_btn_click",
+    "marketing_messages_spend",
+    "place_page_name",
+    "total_postbacks",
+    "total_postbacks_detailed",
+    "total_postbacks_detailed_v4",
+    "unique_conversions",
+    "unique_video_continuous_2_sec_watched_actions",
+    "unique_video_view_15_sec",
+    "video_thruplay_watched_actions",
+    "__module__",
+    "__doc__",
+    "__dict__",
+]
 
 class AdsInsightStream(Stream):
     name = "adsinsights"
@@ -44,7 +75,7 @@ class AdsInsightStream(Stream):
                         th.StringType()
                     )
                     for field in list(AdsActionStats.Field.__dict__)
-                    if field not in ["__module__", "__doc__", "__dict__"]
+                    if field not in EXCLUDED_FIELDS
                 ]
                 return th.ArrayType(
                     th.ObjectType(
@@ -54,7 +85,7 @@ class AdsInsightStream(Stream):
             if "AdsHistogramStats" in d_type:
                 sub_props = []
                 for field in list(AdsHistogramStats.Field.__dict__):
-                    if field not in ["__module__", "__doc__", "__dict__"]:
+                    if field not in EXCLUDED_FIELDS:
                         clean_field = field.replace("field_", "")
                         if AdsHistogramStats._field_types[clean_field] == "string":
                             sub_props.append(
@@ -73,7 +104,7 @@ class AdsInsightStream(Stream):
         properties: List[th.Property] = []
         columns = list(AdsInsights.Field.__dict__)[1:]
         for field in columns:
-            if field in ["__module__", "__doc__", "__dict__"]:
+            if field in EXCLUDED_FIELDS:
                 continue
             properties.append(
                 th.Property(field, self._get_datatype(field))
@@ -81,7 +112,11 @@ class AdsInsightStream(Stream):
         return th.PropertiesList(*properties).to_dict()
     
     def _initialize_client(self):
-        FacebookAdsApi.init(access_token=self.config["access_token"], timeout=300)
+        FacebookAdsApi.init(
+            access_token=self.config["access_token"],
+            timeout=300,
+            api_version=self.config["api_version"],
+        )
         user = fb_user.User(fbid="me")
 
         account_id = self.config["account_id"]
@@ -91,30 +126,28 @@ class AdsInsightStream(Stream):
             raise Exception(msg)
 
     def _run_job_to_completion(self, params):
-        import time
-
         job = self.account.get_insights(
             params=params,
             is_async=True,
         )
         status = None
         time_start = time.time()
-        sleep_time = 10
+        sleep_time = 5
         INSIGHTS_MAX_WAIT_TO_START_SECONDS = 5 * 60
         INSIGHTS_MAX_WAIT_TO_FINISH_SECONDS = 30 * 60
-        INSIGHTS_MAX_ASYNC_SLEEP_SECONDS = 5 * 60
         while status != "Job Completed":
             duration = time.time() - time_start
             job = job.api_get()
             status = job["async_status"]
-            percent_complete = job["async_percent_completion"]
+            percent_complete = job[AdReportRun.Field.async_percent_completion]
 
             job_id = job["id"]
             self.logger.info("%s, %d%% done", status, percent_complete)
 
             if status == "Job Completed":
                 return job
-
+            if status == "Job Failed":
+                raise Exception(dict(job))
             if duration > INSIGHTS_MAX_WAIT_TO_START_SECONDS and percent_complete == 0:
                 pretty_error_message = (
                     "Insights job {} did not start after {} seconds. "
@@ -138,8 +171,6 @@ class AdsInsightStream(Stream):
 
             self.logger.info("sleeping for %d seconds until job is done", sleep_time)
             time.sleep(sleep_time)
-            if sleep_time < INSIGHTS_MAX_ASYNC_SLEEP_SECONDS:
-                sleep_time = 2 * sleep_time
 
     def _get_selected_columns(self):
         return [
